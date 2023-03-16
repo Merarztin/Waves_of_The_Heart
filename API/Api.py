1b4b11874c2b3145b3b2fab593ed9ddeab7bd63f
marwa el bachi <elbachi.marwa@gmail.com>
2023-03-16 14:58:17 +0100

    final changes in the API

--- API/Api.py
+++ API/Api.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 import pickle
 import librosa
+import soundfile as sf
 import numpy as np
 from API.prepoc import extract_features
 import os
@@ -22,15 +23,28 @@ model_path =  os.path.join("API","model.pkl")
 with open(model_path, 'rb') as f:
     app.state.model = pickle.load(f)
 
+
+@app.post('/upload_wav_file')
+def receive_file(file: bytes = File(...)):
+    
+    with open('API/temp/audio.wav','wb') as audio:  
+        audio.write(file)        
+        audio.close()  
+    
+    return 'got it'
+
 @app.get('/predict/')
 async def predict():
 
     # Save audio file to temporary folder
-    #with open(f'temp/audio.wav', 'wb') as f:
-    #    f.write(await file.read())
+    #with open(f'API/temp/audio.wav', 'wb') as f:
+    #   f.write(await file.read())
 
     #audio = extract_features('API/temp/audio.wav')
-    audio = extract_features('API/temp/audio.wav')
+    audio_file = 'API/temp/audio.wav'
+    
+    audio = extract_features(audio_file)
+
 
     # predict the class using pre-trained model
     y_pred = app.state.model.predict(audio)
